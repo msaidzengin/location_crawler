8c94725b6ce71e36aaf0f58d37dac7b7c68ff2fc
msaidzengin <m.zengin@etu.edu.tr>
2019-12-21 13:12:10 +0300

    adding tripadvisor result

--- tripadvisor/tripadvisor.py
+++ tripadvisor/tripadvisor.py
@@ -1,7 +1,7 @@
-from  selenium import webdriver
+from selenium import webdriver
 import json
 
-path = executable_path=r"/home/msaidzengin/chromedriver"
+path = executable_path = r"/home/msaidzengin/chromedriver"
 url = "https://www.tripadvisor.com.tr/Attractions-g298656-Activities-Ankara.html#ATTRACTION_SORT_WRAPPER"
 result_name = "result_tripadvisor.json"
 
@@ -11,15 +11,15 @@ driver.get(url)
 place_data = []
 titles = driver.find_elements_by_class_name("listing_title")
 for title in titles:
-        place_data.append(title.text)
+    place_data.append(title.text)
 
 buttons = driver.find_elements_by_css_selector("a.pageNum.taLnk")
 buttons[0].click()
 
-for i in range(1,8):
+for i in range(1, 8):
     titles = driver.find_elements_by_class_name("listing_title")
     for title in titles:
-            place_data.append(title.text)
+        place_data.append(title.text)
 
     buttons = driver.find_elements_by_css_selector("a.pageNum.taLnk")
     if i > 4:
@@ -29,7 +29,7 @@ for i in range(1,8):
 
 titles = driver.find_elements_by_class_name("listing_title")
 for title in titles:
-        place_data.append(title.text)
+    place_data.append(title.text)
 
 driver.quit()
 
